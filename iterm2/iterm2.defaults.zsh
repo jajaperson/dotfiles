1b464697f7f01532f4919842490e9b33255a2ae1
James Jensen <jamesarthurjensen@gmail.com>
2022-01-06 00:57:50 +0800

    Confirm migration to alacritty
    Screw you, iterm2!

--- iterm2/iterm2.defaults.zsh
+++
@@ -1,8 +0,0 @@
-# Defaults for iterm2.app
-#
-
-# Specify the preferences directory
-defaults write com.googlecode.iterm2 PrefsCustomFolder -string "$DOTFILES/iterm2"
-
-# Tell iTerm2 to use the custom preferences in the directory
-defaults write com.googlecode.iterm2 LoadPrefsFromCustomFolder -bool true
